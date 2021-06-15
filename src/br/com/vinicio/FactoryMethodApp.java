21895bc0e6e84d0b1429cd1c70e52ff598da12be
vinicio <vinicioferreira@sonner.com.br>
2021-06-15 08:07:42 -0300

    fix

--- src/br/com/vinicio/FactoryMethodApp.java
+++ src/br/com/vinicio/FactoryMethodApp.java
@@ -4,17 +4,36 @@ import br.com.vinicio.factory.LogisticaMarinha;
 import br.com.vinicio.factory.LogisticaViaria;
 import br.com.vinicio.model.Transporte;
 
+import java.util.Scanner;
+
 public class FactoryMethodApp {
 
-    public static void main(String[] args) {
-        LogisticaMarinha logisticaMarinha = new LogisticaMarinha();
-        Transporte transporteMarinho = logisticaMarinha.criarTransporte();
-        String resultadoEntregaMarinha = transporteMarinho.entregar();
-        System.out.println(resultadoEntregaMarinha);
+    private static Transporte transporte;
 
-        LogisticaViaria logisticaViaria = new LogisticaViaria();
-        Transporte transporteViario = logisticaViaria.criarTransporte();
-        String resultadoEntregaViaria = transporteViario.entregar();
-        System.out.println(resultadoEntregaViaria);
+    public static void main(String[] args) {
+        String tipoTransporte = escolherTransporte();
+        criarTransporte(tipoTransporte);
+        realizarEntrega();
+    }
+
+    private static String escolherTransporte() {
+        System.out.println("Escolha 1 para Navio e 2 para Caminhao");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
+    private static void criarTransporte(String tipoTransporte) {
+        if(tipoTransporte.equals("1")){
+            LogisticaMarinha logisticaMarinha = new LogisticaMarinha();
+            transporte = logisticaMarinha.criarTransporte();
+        } else if(tipoTransporte.equals("2")){
+            LogisticaViaria logisticaViaria = new LogisticaViaria();
+            transporte = logisticaViaria.criarTransporte();
+        }
+    }
+
+    public static void realizarEntrega(){
+        String resultadoEntrega = transporte.entregar();
+        System.out.println(resultadoEntrega);
     }
 }

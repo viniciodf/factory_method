8d44a658009b2edaa78fb4139cd59f5cb3177aa5
vinicio <vinicioferreira@sonner.com.br>
2021-06-15 08:01:02 -0300

    fix

--- src/br/com/vinicio/factory/Logistica.java
+++ src/br/com/vinicio/factory/Logistica.java
@@ -5,7 +5,7 @@ import br.com.vinicio.model.Transporte;
 public abstract class Logistica {
     public abstract Transporte criarTransporte();
 
-    void realizarEntrega(){
+    public void realizarEntrega(){
         Transporte transporte = criarTransporte();
         String resultadoDaEntrega = transporte.entregar();
     }
